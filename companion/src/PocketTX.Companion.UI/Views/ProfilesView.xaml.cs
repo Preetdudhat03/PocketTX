72fa016874b4396b59c6df9b2ad98b6cb120d4bd
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 20:47:33 +0530

    chore: update ProfilesView.xaml.cs, SettingsView.xaml

---
+++ companion/src/PocketTX.Companion.UI/Views/ProfilesView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class ProfilesView : UserControl
+{
+    public ProfilesView()
+    {
+        InitializeComponent();
+    }
+}

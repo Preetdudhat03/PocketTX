94961b2807c197ce9ac3bb450340909f9de61a3a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 20:47:39 +0530

    chore: update LogsView.xaml, LogsView.xaml.cs, SettingsView.xaml.cs

---
+++ companion/src/PocketTX.Companion.UI/Views/LogsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class LogsView : UserControl
+{
+    public LogsView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ companion/src/PocketTX.Companion.UI/Views/SettingsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}

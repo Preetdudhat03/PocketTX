2911e0892263d7ce08cf57675d75ebe5bb2e613a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 21:46:49 +0530

    chore: update companion/src/PocketTX.Companion.UI/Views/MainWindow.xaml.cs

--- companion/src/PocketTX.Companion.UI/Views/MainWindow.xaml.cs
+++ companion/src/PocketTX.Companion.UI/Views/MainWindow.xaml.cs
@@ -1,13 +1,22 @@
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using PocketTX.Companion.UI.ViewModels;
 
 namespace PocketTX.Companion.UI.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow(MainViewModel viewModel)
+    public MainWindow()
     {
         InitializeComponent();
+        if (Application.Current is App app)
+        {
+            DataContext = app.Services.GetRequiredService<MainViewModel>();
+        }
+    }
+
+    public MainWindow(MainViewModel viewModel) : this()
+    {
         DataContext = viewModel;
     }
 }

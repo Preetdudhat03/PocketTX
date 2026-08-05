1a363e7e74c2d8fc3b15fc2bcc3afc8870ab7b93
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 20:47:16 +0530

    test: update TestControllerView.xaml, TestControllerView.xaml.cs

---
+++ companion/src/PocketTX.Companion.UI/Views/TestControllerView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class TestControllerView : UserControl
+{
+    public TestControllerView()
+    {
+        InitializeComponent();
+    }
+}

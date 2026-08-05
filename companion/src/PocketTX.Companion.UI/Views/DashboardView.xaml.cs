1d9decb34816b161f1550b7a16fb2d1ed6b24518
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 20:47:11 +0530

    chore: update DashboardView.xaml, DashboardView.xaml.cs

---
+++ companion/src/PocketTX.Companion.UI/Views/DashboardView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}

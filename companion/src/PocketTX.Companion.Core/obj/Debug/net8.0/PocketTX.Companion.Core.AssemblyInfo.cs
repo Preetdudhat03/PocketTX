1de91e183eac951b99a080965abb764fba412c3f
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 21:07:11 +0530

    chore: update 90 repository files

--- companion/src/PocketTX.Companion.Core/obj/Debug/net8.0/PocketTX.Companion.Core.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Core/obj/Debug/net8.0/PocketTX.Companion.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa280bf0cf4fea222a828e0e3c04db75118b064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b26feff719cb84848ed60fb76845937b69565c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- companion/src/PocketTX.Companion.VirtualController/obj/Debug/net8.0/PocketTX.Companion.VirtualController.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.VirtualController/obj/Debug/net8.0/PocketTX.Companion.VirtualController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa280bf0cf4fea222a828e0e3c04db75118b064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c9e8425cda4abf614cc3197cd1ea8cc8965b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

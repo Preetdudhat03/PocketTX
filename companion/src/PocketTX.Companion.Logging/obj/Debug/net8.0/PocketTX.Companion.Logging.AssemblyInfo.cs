4ac609ee9317ab73f670d197bb715596f49912f3
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 21:27:56 +0530

    chore: update 145 repository files

--- companion/src/PocketTX.Companion.Logging/obj/Debug/net8.0/PocketTX.Companion.Logging.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Logging/obj/Debug/net8.0/PocketTX.Companion.Logging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Logging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b69baf1d9f6ef8e27af7f3dcd725c7f9d897a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e97b370f59628c804dd70a35816c9fd5e7f49f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Logging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Logging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

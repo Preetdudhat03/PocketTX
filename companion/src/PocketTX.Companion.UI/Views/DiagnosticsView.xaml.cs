4c33d6e8924075abdcc2ac68205e8a360917cb73
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-05 20:47:22 +0530

    chore: update DiagnosticsView.xaml, DiagnosticsView.xaml.cs

---
+++ companion/src/PocketTX.Companion.UI/Views/DiagnosticsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PocketTX.Companion.UI.Views;
+
+public partial class DiagnosticsView : UserControl
+{
+    public DiagnosticsView()
+    {
+        InitializeComponent();
+    }
+}

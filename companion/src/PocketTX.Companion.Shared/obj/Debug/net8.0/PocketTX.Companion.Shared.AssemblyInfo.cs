64834e811b1d1d7a553da844f8bcc3fc2809aac9
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-07 21:43:47 +0530

    chore: update PocketTX.Companion.Shared.AssemblyInfo.cs, PocketTX.Companion.Shared.AssemblyInfoInputs.cache, PocketTX.Companion.Shared.sourcelink.json

--- companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff1d5f598fcf8462f4ea16683e212a98bbfcaa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8857187681facb22a9a4ad18f4f0a1964052c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

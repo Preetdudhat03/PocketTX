ca0d7ae16e9f8385cec94a1471c2ab32e84a6323
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-07 21:07:25 +0530

    test: update 16 test files

--- companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbc2b04e7c156109a7a114bc150028d65a42e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d959b6277a2a0cb355a7f4af1e798343bfd43f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

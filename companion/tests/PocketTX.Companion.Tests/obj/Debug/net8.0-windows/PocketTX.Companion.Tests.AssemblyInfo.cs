e8e5950034e62e3f31eb18e7885084032d33934e
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-09 13:41:37 +0530

    test: update 55 test files

--- companion/tests/PocketTX.Companion.Tests/obj/Debug/net8.0-windows/PocketTX.Companion.Tests.AssemblyInfo.cs
+++ companion/tests/PocketTX.Companion.Tests/obj/Debug/net8.0-windows/PocketTX.Companion.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683d0cc4544545567592e453a86186864e88a6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fd56d53716dbc385c5f7e6ea7bc766227e1934")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

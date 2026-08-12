5ed1c5d09a7323ec9d1d423588201f711583d239
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-12 23:10:57 +0530

    chore: update android_app/lib/features/controller/presentation/screens/controller_screen.dart

--- android_app/lib/features/controller/presentation/screens/controller_screen.dart
+++ android_app/lib/features/controller/presentation/screens/controller_screen.dart
@@ -50,11 +50,11 @@ class _ControllerScreenState extends ConsumerState<ControllerScreen> {
                     //final dynamicGimbalSize = (constraints.maxHeight * 0.82).clamp(190.0, 260.0);
                     //final sideWidth = (constraints.maxWidth * 0.35).clamp(dynamicGimbalSize + 16.0, 320.0);
 
-                    final dynamicGimbalSize = (constraints.maxHeight * 0.90).clamp(220.0, 300.0);
-                    final sideWidth = (constraints.maxWidth * 0.38).clamp(dynamicGimbalSize + 16.0, 360.0);
+                    //final dynamicGimbalSize = (constraints.maxHeight * 0.90).clamp(220.0, 300.0);
+                    //final sideWidth = (constraints.maxWidth * 0.38).clamp(dynamicGimbalSize + 16.0, 360.0);
 
-                    //final dynamicGimbalSize = (constraints.maxHeight * 0.94).clamp(240.0, 340.0);
-                    //final sideWidth = (constraints.maxWidth * 0.40).clamp(dynamicGimbalSize + 16.0, 400.0);
+                    final dynamicGimbalSize = (constraints.maxHeight * 0.94).clamp(240.0, 340.0);
+                    final sideWidth = (constraints.maxWidth * 0.40).clamp(dynamicGimbalSize + 16.0, 400.0);
 
                     //final dynamicGimbalSize = (constraints.maxHeight * 0.98).clamp(260.0, 420.0);
                     //final sideWidth = (constraints.maxWidth * 0.42).clamp(dynamicGimbalSize + 16.0, 450.0);

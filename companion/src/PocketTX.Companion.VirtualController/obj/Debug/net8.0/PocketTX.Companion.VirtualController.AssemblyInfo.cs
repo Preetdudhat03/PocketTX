7d8ae230c40aa063602cb8a4ce5f011237146356
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-13 23:52:49 +0530

    chore: update 110 repository files

--- companion/src/PocketTX.Companion.VirtualController/obj/Debug/net8.0/PocketTX.Companion.VirtualController.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.VirtualController/obj/Debug/net8.0/PocketTX.Companion.VirtualController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea26f9a039e4212312380acec598cf2ee1169af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46670f174ade6e1dfe553f5d77daf790ab24b454")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.VirtualController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

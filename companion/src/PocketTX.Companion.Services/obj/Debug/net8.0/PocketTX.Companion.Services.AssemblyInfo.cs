4dfc5a8b72cf82885e96b189daa31d491b0a375d
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-14 13:11:11 +0530

    test: update 18 test files

--- companion/src/PocketTX.Companion.Services/obj/Debug/net8.0/PocketTX.Companion.Services.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Services/obj/Debug/net8.0/PocketTX.Companion.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee96176406a11f5381e2553f8b8e97656c383ca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b788190ade618e8aba127fbfd97ce73d42370dba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

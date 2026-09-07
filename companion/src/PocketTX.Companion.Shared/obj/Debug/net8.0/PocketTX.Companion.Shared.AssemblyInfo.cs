1c72ed8f3651f9e5e422bb9285fbccb0087657e7
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-07 20:26:35 +0530

    chore: update 31 repository files

--- companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
+++ companion/src/PocketTX.Companion.Shared/obj/Debug/net8.0/PocketTX.Companion.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc045774ac97f66caf72f7a823ab02efff6225e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4bffc5ab44fd39886275f1cca3f6b87689617e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketTX.Companion.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
